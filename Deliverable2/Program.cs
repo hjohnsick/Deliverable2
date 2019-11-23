2050d184337c4d5adad4292d10dcfec74b2941b1
heather <hjohnsick@yahoo.com>
2019-11-23 15:14:39 -0500

    First commit for deliverable two

---
+++ Deliverable2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Deliverable2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+
+                int checksum = 0;
+                string message = "";
+
+                Console.WriteLine("What is your message?");
+
+                string input = Console.ReadLine().ToUpper();
+
+                for (int i = 0; i < input.Length; i++)
+                {
+                    int chVal = (int)input[i];
+                    int alphaValue = chVal - 64;
+
+                    checksum += chVal;
+                    message += alphaValue.ToString();
+
+                    if (i < input.Length - 1)
+                    {
+                        message += "-";
+                    }
+                }
+
+                Console.WriteLine("Your encoded message is " + message);
+                Console.WriteLine("Message checksum is " + checksum);
+            }
+        }
+    }
+
+}
+
+    
